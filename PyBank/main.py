d4fb0093aa39eb4773ab220ce4ff1522edc7c474
Kelvin Nguyen <k.nguyen.pwc@gmail.com>
2021-11-27 13:03:09 +1100

    Final script

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,50 +1,84 @@
+# This script is for PyBank
+
+# Import dependencies
 import os
 import csv
 
 fileinputpath = os.path.join(".","Resources","budget_data.csv")
+
+# Declaring variables for analysis
 totalmonths = 0
-nettotal = 0 
-changeprofitlost =[]
-greatestinscreaseprofit = 0 
-greatestdecreaseprofit = 0 
-lastprofitlost = 0
-
-with open(fileinputpath,"r") as csvfileinput:
-    filecontent = csv.reader(csvfileinput)
-    header = next(csvfileinput)
-    for row in filecontent:
-        currentamount = int(row[1])
-        totalmonths += 1
-        nettotal += currentamount
-        if totalmonths > 1:
-            currentchange = currentamount- lastprofitlost
-            changeprofitlost.append(currentchange)
-        lastprofitlost=currentamount
-        
-        if greatestinscreaseprofit < currentchange:
-            greatestinscreaseprofit = currentchange
-            greatprofitmonth = row[0]
-            
-        if greatestdecreaseprofit > currentchange:
-            greatestdecreaseprofit = currentchange
-            greatlostmonth = row[0]
-
 totalchange = 0
+nettotal = 0 
+change_PL =[]
+greatest_inscrease_profit = 0 
+greatest_decrease_profit = 0 
+last_month_PL = 0
 
-for change in changeprofitlost:
-    totalchange += change  
-
-averagechange = totalchange / (totalmonths -1)
+with open(fileinputpath,"r") as csv_input:
+    filecontent = csv.reader(csv_input)
+    
+    # skip header because there's no header row
+    header = next(csv_input)
+    
+    # loop through the file content to get data
+    for row in filecontent:
+        current_amount = int(row[1])
         
-fileoutputpath = os.path.join (".","Analysis","PerformanceSummary.txt")
+        # adding up net profit and counting months for the whole period
+        totalmonths += 1
+        nettotal += current_amount
+        
+        # get changes in profit from second month and add to a list (first month has no data available to compare )
+        if totalmonths > 1:
+            current_change = current_amount - last_month_PL
+            change_PL.append(current_change)
+                 
+            # set current change in profit as a new greatest increase in profit if it is higher and remember the current month 
+            if greatest_inscrease_profit < current_change:
+                greatest_inscrease_profit = current_change
+                best_month = row[0]
 
+             # set current change in profit as a new greatest decrease in profit if it is lower and remember the current month 
+            if greatest_decrease_profit > current_change:
+                greatest_decrease_profit = current_change
+                worst_month = row[0]
+                
+        # remember last month change of profit
+        last_month_PL=current_amount                    
+        
+        
+# add up changes in profit
+for change in change_PL:
+    totalchange += change  
+  
+average_change = totalchange / (totalmonths - 1)
+
+dashes = "----------------------------"
+# define a function to print out result nicely - justified margin
+def print_justify(text, variable):        
+    length = len(dashes)
+    gap = length - len(text) - len(str(variable))
+    return text + (" " * gap) + str(variable)
+
+
+# assign result ouput to a list
+result = [] 
+result.append("Financial Analysis")
+result.append(dashes)
+result.append(print_justify("Total Months: ", totalmonths))
+result.append(print_justify("Total: " ,  "$" + str(nettotal)))
+result.append(print_justify("Average  Change:", "$" + str(round(average_change,2))))
+result.append("Greatest Increase in Profits: "+ best_month + " ($" + str(greatest_inscrease_profit) + ")")
+result.append("Greatest Decrease in Profits: "+ worst_month + " ($" + str(greatest_decrease_profit) + ")")
+
+# print result to screen
+for item in result:
+    print(item)
+
+# writing result to output file
+fileoutputpath = os.path.join (".","Analysis","PerformanceSummary.txt")
 with open(fileoutputpath,"w") as outputfile:
     outputcontent = csv.writer(outputfile)
-    outputcontent.writerow(["Financial Analysis"])
-    outputcontent.writerow(["----------------------------"])
-    outputcontent.writerow(["Total Months: "+ str(totalmonths)])
-    outputcontent.writerow(["Total: $" + str(nettotal)])
-    outputcontent.writerow(["Average  Change: $ "+ str(round(averagechange,2))])
-    outputcontent.writerow(["Greatest Increase in Profits: "+ greatprofitmonth + " ($" + str(greatestinscreaseprofit) + ")"])
-    outputcontent.writerow(["Greatest Decrease in Profits: "+ greatlostmonth + " ($" + str(greatestdecreaseprofit) + ")"])
-    
+    for item in result:
+        outputcontent.writerow([item])
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,45 +1,80 @@
+# This script is for PyPoll
+
+# import dependencies
 import os
 import csv
 
+# Open input file to read in data
 file_path = os.path.join(".", "Resources","election_data.csv")
 
 with open(file_path,"r") as input_file:
     content = csv.reader(input_file)
+    
+    # skip the header 
     header = next(content)
     
+    # setting up variables
     total_votes =  0
     candidates = {}
-    for row in content
+    
+    # looping through file content
+    for row in content:
+        
+        # count the total vote
         total_votes += 1
-        if row[2] is not in candidates:
-            candidates
+        
+        # if candidate is not yet in the dictionary, set count to 0
+        if row[2] not in candidates:
+            candidates[row[2]] = 0
+            
+        # count the total vote for the current candidate
+        candidates[row[2]] += 1
 
-* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+dashes = "-------------------------" 
 
-  * The total number of votes cast
+# define a function to print out result nicely - justified margin
+def print_justify(text, variable):        
+    length = len(dashes)
+    gap = length - len(text) - len(str(variable))
+    return text + (" " * gap) + str(variable)
 
-  * A complete list of candidates who received votes
+# Writing result to a resuable list
+result = []
+result.append("Election Results")
+result.append(dashes)
+result.append(print_justify("Total Votes: ",total_votes))
+result.append(dashes)
 
-  * The percentage of votes each candidate won
+# calculate percentage votes for each candidate
+for name in candidates:
+    candidate_votes = candidates[name]
+    percentage_votes = candidate_votes / total_votes * 100
+    result.append(print_justify(name + ": " , f"%.3f%% (%d)" %(percentage_votes , candidate_votes)))
 
-  * The total number of votes each candidate won
+result.append(dashes)
 
-  * The winner of the election based on popular vote.
+# setting up variables
+highest_vote = 0
+winner = ""
 
-* As an example, your analysis should look similar to the one below:
+# loop through each candidate to determine the candidate with highest votes
+for name in candidates:
+    if candidates[name] > highest_vote:
+        highest_vote = candidates[name]
+        winner = name       
+result.append(print_justify("Winner: ", winner))
+result.append(dashes)
+# writing result ended
 
-  ```text
-  Election Results
-  -------------------------
-  Total Votes: 3521001
-  -------------------------
-  Khan: 63.000% (2218231)
-  Correy: 20.000% (704200)
-  Li: 14.000% (492940)
-  O'Tooley: 3.000% (105630)
-  -------------------------
-  Winner: Khan
-  -------------------------
-  ```
-
-* In addition, your final script should both print the analysis to the terminal and export a text file with the results.
+# print out result 
+for item in result:
+    print(item)
+              
+# writing result to output file        
+file_path = os.path.join(".", "Analysis","election_result.txt")
+    
+with open(file_path,"w") as output_file:
+    content = csv.writer(output_file)
+    for item in result:
+        content.writerow([item])
+    
